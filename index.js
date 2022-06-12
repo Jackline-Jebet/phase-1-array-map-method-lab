8be1ac503054c81aae1473cb99457f04a9ca194d
Jackline Jebet <jebetjackline2016@gmail.com>
2022-06-12 10:36:41 +0300

    commit tree e36b5aedf9b6688e52797df7bca417ecbeda310b

--- index.js
+++ index.js
@@ -8,9 +8,29 @@ const tutorials = [
   'Immutable State and Pure Functions',
   'what is the difference between == and ===?',
   'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  'what is JSONP?',
+
+
+    "What Does The This Keyword Mean?",
+    "What Is The Constructor OO Pattern?",
+    "Implementing Blockchain Web API",
+    "The Test Driven Development Workflow",
+    "What Is NaN And How Can We Check For It",
+    "What Is The Difference Between StopPropagation And PreventDefault?",
+    "Immutable State And Pure Functions",
+    "What Is The Difference Between == And ===?",
+    "What Is The Difference Between Event Capturing And Bubbling?",
+    "What Is JSONP?"
 ];
 
 const titleCased = () => {
   return tutorials
 }
+    const newTitleCased = tutorials.map(tutorials => {
+        const tutorial = tutorials.split('');
+        const tutorialCap = tutorial.map(tutor => tutor[0].toUpperCase() + tutor.slice(1, tutor.length));
+        const cased = tutorialCap.join('');
+        return cased;
+
+    });
+    console.log(newTitleCased);
